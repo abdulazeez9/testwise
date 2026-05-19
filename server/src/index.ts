b3b5603fed8dc39bae241fba8aebfab6490ba60a
Abdulazeez Muritador <107861280+abdulazeez9@users.noreply.github.com>
2026-05-19 14:28:43 +0100

    Change server listen address to '0.0.0.0'

--- server/src/index.ts
+++ server/src/index.ts
@@ -30,6 +30,6 @@ app.use(errorHandler);
 const server = createServer(app);
 webSocketService.init(server);
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
